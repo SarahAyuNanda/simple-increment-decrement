ac2e29588f6b0da7dac786f2ec1c92e390776768
SarahAyuNanda <nanda.sarahayu@gmail.com>
2020-09-23 22:01:29 +0700

    [CREATE] Toggle component

---
+++ src/components/Toggle.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../assets/Toggle.css'
+
+const Toggle = (props) => {
+    const result = props.result
+    const [showToggle, setShowToggle] = useState(false)
+
+    const toggleButton = () => {
+        setShowToggle(!showToggle)
+    }
+
+    return (
+        <div className="toggle">
+            {showToggle ?
+                <span id="show">{result}</span>
+                :
+                <span id="hide">{result}</span>}
+            <button type="button" class="btn btn-dark" onClick={toggleButton}>
+                {showToggle ? "Hide" : "Show"}
+            </button>
+        </div>
+    );
+};
+
+export default Toggle;
