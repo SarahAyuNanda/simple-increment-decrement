2186a3a9afac669d52299de5b6037ac6dbe65135
SarahAyuNanda <nanda.sarahayu@gmail.com>
2020-09-24 13:43:57 +0700

    [CREATE] Decrement component

---
+++ src/components/Decrement.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import '../assets/Decrement.css'
+
+const Decrement = (props) => {
+    const result = props.result
+    const setResult = props.setResult
+
+    const decrement = () => {
+        let number = result - 1
+        setResult(number)
+    }
+    return (
+        <div>
+            <button id="decrement" type="button" class="btn btn-danger" onClick={decrement}>Decrement</button>
+        </div>
+    );
+};
+
+export default Decrement;
