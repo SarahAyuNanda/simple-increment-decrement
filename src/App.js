a9c1bcb4dcd8e1f63a54f23cd1bcaa22f4369c6e
SarahAyuNanda <nanda.sarahayu@gmail.com>
2020-09-24 13:43:22 +0700

    [ADD] Increment and decrement button

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import React, { useState } from 'react';
 import Toggle from './components/Toggle';
-// import { Button } from 'react-bootstrap';
+import Increment from './components/Increment';
+import Decrement from './components/Decrement';
+
 import './assets/App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
   const [result, setResult] = useState(0)
 
-  const increment = () => {
-    let number = result + 1
-    setResult(number)
-  }
-  const decrement = () => {
-    let number = result - 1
-    setResult(number)
-  }
-
   return (
     <div className="App">
-      <Toggle result={result} setResult={setResult}/>
+      <Toggle result={result} setResult={setResult} />
       <div className="buttons">
-        <button type="button" class="btn btn-primary" onClick={increment}>Increment</button>
-        <button type="button" class="btn btn-danger" onClick={decrement}>Decrement</button>
+        <Increment result={result} setResult={setResult} />
+        <Decrement result={result} setResult={setResult}/>
       </div>
     </div>
   );
