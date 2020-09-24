b19fee6b9a519ea1f8ae938eb60c480315713380
SarahAyuNanda <nanda.sarahayu@gmail.com>
2020-09-24 13:17:01 +0700

    [EDIT] Replace toggle button with component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
+import Toggle from './components/Toggle';
 // import { Button } from 'react-bootstrap';
-import './App.css'
+import './assets/App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
   const [result, setResult] = useState(0)
-  const [showToggle, setShowToggle] = useState(false)
 
   const increment = () => {
     let number = result + 1
@@ -15,28 +15,14 @@ const App = () => {
     let number = result - 1
     setResult(number)
   }
-  const reset = () => {
-    setResult(0)
-  }
-  const toggle = () => {
-    setShowToggle(!showToggle)
-  }
+
   return (
     <div className="App">
-      { showToggle ? <span id="show">{result}</span> : <span id="hide">{result}</span>}
-      <button type="button" class="btn btn-dark" onClick={toggle}>
-        {showToggle ? 'Hide number' : 'Show number'}
-      </button>
-
-      {/* USING REACT-BOOTSTRAP */}
-      {/* <Button onClick={increment}>Increment</Button> */}
-
-      {/* USING BOOTSTRAP CSS */}
+      <Toggle result={result} setResult={setResult}/>
       <div className="buttons">
         <button type="button" class="btn btn-primary" onClick={increment}>Increment</button>
         <button type="button" class="btn btn-danger" onClick={decrement}>Decrement</button>
       </div>
-      <button type="button" class="btn btn-warning" onClick={reset}>Reset</button>
     </div>
   );
 };
