1e3aa6a99407d51df9bba2d1575736f30cfabdb3
SarahAyuNanda <nanda.sarahayu@gmail.com>
2020-09-24 20:36:56 +0700

    [DELETE] Increment and decrement button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import Toggle from './components/Toggle';
-import Increment from './components/Increment';
-import Decrement from './components/Decrement';
 
 import './assets/App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -12,10 +10,6 @@ const App = () => {
   return (
     <div className="App">
       <Toggle result={result} setResult={setResult} />
-      <div className="buttons">
-        <Increment result={result} setResult={setResult} />
-        <Decrement result={result} setResult={setResult} />
-      </div>
     </div>
   );
 };
