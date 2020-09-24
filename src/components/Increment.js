c97476861606f59d976ff5334d18a038de5adf89
SarahAyuNanda <nanda.sarahayu@gmail.com>
2020-09-24 13:44:18 +0700

    [CREATE] Increment component

---
+++ src/components/Increment.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import '../assets/Increment.css'
+
+const Increment = (props) => {
+    const result = props.result
+    const setResult = props.setResult
+
+    const increment = () => {
+        let number = result + 1
+        setResult(number)
+    }
+    return (
+        <div>
+            <button id="increment" type="button" class="btn btn-primary" onClick={increment}>Increment</button>
+        </div>
+    );
+};
+
+export default Increment;
