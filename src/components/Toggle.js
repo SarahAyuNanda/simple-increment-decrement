15a9b29cc45fe1bd18ce549cd52e4e18416df4ab
SarahAyuNanda <nanda.sarahayu@gmail.com>
2020-09-25 12:25:15 +0700

    [EDIT] Toggle type

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import Reset from './Reset';
+// import Reset from './Reset';
+import Increment from './Increment';
+import Decrement from './Decrement';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../assets/Toggle.css'
@@ -15,16 +17,20 @@ const Toggle = (props) => {
 
     return (
         <div className="Toggle">
+            <input type="checkbox" id="switch" />
+            <label for="switch" onClick={toggleButton}></label>
             {showToggle ?
-                <span id="show">{result}</span>
+                <div className="wrapper">
+                    <Decrement result={result} setResult={setResult} />
+                    <span id="show">{result}</span>
+                    <Increment result={result} setResult={setResult} />
+                </div>
                 :
                 <span id="hide">{result}</span>}
-            <div className="buttons">
-                <button id="toggle" type="button" class="btn btn-dark" onClick={toggleButton}>
+
+            {/* <button id="toggle" type="button" class="btn btn-dark" onClick={toggleButton}>
                     {showToggle ? "Hide" : "Show"}
-                </button>
-                <Reset setResult={setResult} />
-            </div>
+                </button> */}
         </div>
     );
 };
